0ea3571dad582f48788ecd0c78b60077fbe54957
Henry Hieu Nguyen <hieunugent@gmail.com>
2021-09-14 12:52:26 -0700

    update

--- src/image/image.js
+++ src/image/image.js
@@ -1,20 +1,33 @@
+import page1 from "./Aborn/lover tully 20210901 website-.jpg";
+import page2 from "./Aborn/lover tully 20210901 website-3.jpg";
+import page3 from "./Aborn/lover tully 20210901 website-4.jpg";
+import page4 from "./Aborn/lover tully 20210901 website-5.jpg";
+import page5 from "./Aborn/lover tully 20210901 website-6.jpg";
+import page6 from "./Aborn/lover tully 20210901 website-7.jpg";
 
-import page1 from "./Aborn/96dpi.jpg";
-import page2 from "./Aborn/96dpi2.jpg";
-import page3 from "./Aborn/96dpi2.jpg";
-
-const listMenu = [
-  {
-    page: "1",
-    itemsImage: page1,
-  },
-  {
-    page: "2",
-    itemsImage: page2,
-  },
-  {
-    page: "3",
-    itemsImage: page3,
-  },
+const listMenu = [{
+        page: "1",
+        itemsImage: page1,
+    },
+    {
+        page: "2",
+        itemsImage: page2,
+    },
+    {
+        page: "3",
+        itemsImage: page3,
+    },
+    {
+        page: "4",
+        itemsImage: page4,
+    },
+    {
+        page: "5",
+        itemsImage: page5,
+    },
+    {
+        page: "6",
+        itemsImage: page6,
+    },
 ];
 export default listMenu;
--- src/menu.js
+++ src/menu.js
@@ -4,32 +4,34 @@ import { makeStyles } from "@material-ui/core/styles";
 
 
 const useStyles = makeStyles((theme) => ({
-  img: {
-    margin: 0,
-    display: "flex",
-    
-    maxWidth: "100%",
-    
-    backgroundColor: "black",
-    alignItems: "center",
-  },
+    img: {
+        margin: 0,
+        display: "flex",
+
+        maxWidth: "100vw",
+
+        backgroundColor: "black",
+        alignItems: "center",
+    },
 }));
 
 
 export default function menu(props) {
     // eslint-disable-next-line react-hooks/rules-of-hooks
     const classes = useStyles();
-    return (
-      <div>
-        <Paper>
-          <figure className={classes.img}>
-            <img
-              className={classes.img}
-              alt="complex"
-              src={props.itemsImage}
-            />
-          </figure>
-        </Paper>
-      </div>
+    return ( <
+        div >
+        <
+        Paper >
+        <
+        figure className = { classes.img } >
+        <
+        img className = { classes.img }
+        alt = "complex"
+        src = { props.itemsImage }
+        /> <
+        /figure> <
+        /Paper> <
+        /div>
     );
 }
