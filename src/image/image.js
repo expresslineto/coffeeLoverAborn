e341d286e5b234d800b4a795e03bee12074facda
hieunugent <hieunugent@gmail.com>
2025-12-14 09:04:50 -0800

    update 12.2025

--- src/image/image.js
+++ src/image/image.js
@@ -1,62 +1,34 @@
-import page1 from "./Aborn/a0 (1).jpg";
-import page2 from "./Aborn/a0 (2).jpg";
-import page3 from "./Aborn/a0 (3).jpg";
-import page4 from "./Aborn/a0 (4).jpg";
-import page5 from "./Aborn/a0 (5).jpg";
-import page6 from "./Aborn/aborn 2025 -05 back (1).jpg";
-
-import page7 from "./Aborn/aborn 2025 -05 back (2).jpg"
-import page8 from "./Aborn/aborn 2025 -05 back (3).jpg"
-
-import page9 from "./Aborn/aborn 2025 -05 back (4).jpg"
-import page10 from "./Aborn/aborn 2025 -05 back (5).jpg"
-import page11 from "./Aborn/aborn 2025 front (1).jpg"
-import page12 from "./Aborn/aborn 2025 front (2).jpg"
-
-
-const listMenu = [{
-        page: "1",
-        itemsImage: page1,
-    },
-    {
-        page: "2",
-        itemsImage: page2,
-    },
-    {
-        page: "3",
-        itemsImage: page3,
-    },
-    {
-        page: "4",
-        itemsImage: page4,
-    },
-    {
-        page: "5",
-        itemsImage: page5,
-    },
-    {
-        page: "6",
-        itemsImage: page6,
-    },{
-        page: "7",
-        itemsImage: page7,
-    },{
-        page: "8",
-        itemsImage: page8,
-    },{
-        page: "9",
-        itemsImage: page9,
-    },{
-        page: "10",
-        itemsImage: page10,
-    },
-    {
-        page: "11",
-        itemsImage: page11,
-    },
-    {
-        page: "12",
-        itemsImage: page12,
-    },
-];
+export function importAllImages(r){
+    let images = {};
+    r.keys().forEach((key) => {
+        const imageName = key.replace('./', '');
+        images[imageName]= r(key);
+    });
+    return images
+}
+export function saveImage(r){
+    function popAny(obj){
+        const keys = Object.keys(obj);
+        if(keys.length ===0) return undefined;
+        const key = keys[0];
+        const value =obj[key];
+        delete obj[key];
+        return value;
+    }
+    let list = [];
+    let key = 0;
+    while (Object.keys(r).length > 0){
+        
+            const value = popAny(r);
+            if (value !== undefined){
+                list.push({page:key, itemsImage:value});
+                key +=1
+            }else 
+               {break}
+       
+    }
+    return list
+}
+const images = importAllImages(require.context('./aborn', false,/\.(jpg)$/));
+const listMenu =saveImage(images);
 export default listMenu;
